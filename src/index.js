902ae4a78ecaadfa36598520d8a99170f7670884
cveta96 <djnik8225@gmail.com>
2023-01-26 15:52:15 +0100

    "1/2 auth"

--- src/index.js
+++ src/index.js
@@ -7,12 +7,17 @@ import reportWebVitals from './reportWebVitals';
 
 import { ContextProvider } from './contexts/ContextProvider';
 
+import { store } from './store';
+import { Provider } from 'react-redux';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ContextProvider>
-      <App />
-    </ContextProvider>
+    <Provider store={store}>
+      <ContextProvider>
+        <App />
+      </ContextProvider>
+    </Provider>
   </React.StrictMode>
 );
 
